1136f66be210826d61a4fd9eb68757a074f0576a
Afsal <thenameisafsalahamad@gmail.com>
2024-09-10 19:03:17 +0530

    implemented popup menu for logout actions with comments

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:notes/views/register_view.dart';
 import 'package:notes/views/verify_email_view.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer' as devtools;
 void main(){
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -66,15 +67,53 @@ class _NotesViewState extends State<NotesView> {
     return Scaffold(
       appBar:  AppBar(title: const Text("Notes UI"),
       actions: [
+        // the logout option in the menu
           PopupMenuButton<MenuAction>(
-            onSelected: (value) => {},
+            onSelected: (value) async {
+              switch(value){
+                case MenuAction.logout:
+                  final confirmation = await LogOut(context);
+                  // devtools.log(confirmation.toString()); // prints the confirmation boolean
+                  if(confirmation){
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil('/login/', (_)=> false); // move to the login page upon logout
+                  }
+                  break;
+              }
+            },
             itemBuilder: (context) { return const [ // itembuilder will return only a list<popupmenuentry<MenuAction>>  type -> note that popupMenuItem is a subclass of popupMenuEntry as it extends popupMenuEntry
                  PopupMenuItem(value: MenuAction.logout, child: Text("logout")) // the value passed here will be passed on to the onSelected() function defined above as the value parameter
-          ];
-          }
+            ];
+           }
           )
-      ],
+        ],
       ),
     );
   }
+}
+
+// implementation of the logout confirmation dialog
+Future<bool> LogOut(BuildContext context){
+  return showDialog<bool>(
+        context : context,
+        builder: (context){
+        return AlertDialog(title: const Text('Logout'),
+            content: const Text("You really want to log out?"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: const Text("Cancel")
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  child: const Text("Log Out")
+                )
+              ],
+            );
+      }
+  ).then((value) => value ?? false); // since this is an optional future we make sure to return a future by checking for the null value return
 }
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:notes/views/login_view.dart';
-import '../firebase_options.dart';
 class RegisterView extends StatefulWidget{
   const RegisterView({super.key});
 
