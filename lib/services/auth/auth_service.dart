e053ba6dddba2f605a0bad0c3947145e29b2a60e
Afsal <thenameisafsalahamad@gmail.com>
2024-09-18 13:09:06 +0530

    renamed repo

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,9 +1,13 @@
 import 'package:notes/services/auth/auth_provider.dart';
 import 'package:notes/services/auth/auth_user.dart';
+import 'package:notes/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider{
   final AuthProvider provider;
   const AuthService(this.provider);
+
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+
   @override
   Future<AuthUser> createUser({required String email, required String password}) {
     return provider.createUser(email: email, password: password);
