cf9d9816b4f999dfe7ac1b505d38d3d501fe7b9f
Afsal <thenameisafsalahamad@gmail.com>
2024-09-21 15:05:54 +0530

    implemented notes view and textfield in ui

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes/services/auth/auth_service.dart';
+import 'package:notes/services/crud/notes_service.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({super.key});
@@ -8,13 +10,96 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+  // keep reference of these two vars to avoid defining them each time - singleton
+  DataBaseNote? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  @override
+  void initState(){
+    _notesService = NotesService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+// update db as the user types data
+  void _textControllerListener() async{
+    final note = _note;
+    if(note == null){
+      return;
+    }
+    final text = _textController.text;
+    await _notesService.updateNote(note: note, text: text);
+  }
+
+  void _setupTextControllerListener(){
+    // refresh the listener
+    _textController.removeListener(_textControllerListener);
+    _textController.addListener(_textControllerListener);
+  }
+
+  Future<DataBaseNote> createNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final user =
+        AuthService.firebase().currentUser!; // crash if user doesn't exist
+    final email = user.email!;
+    final owner = await _notesService.getUser(email: email); // get the owner
+    return await _notesService.createNote(owner: owner); // create the note
+  }
+
+  void deleteNoteIfEmpty() {
+    final note = _note;
+    //  if no text entered, delete it
+    if (_textController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void saveNoteIfNotempty() async {
+    final note = _note;
+    final text = _textController.text;
+    // if there is text, save it
+    if (note != null && text.isNotEmpty) {
+      await _notesService.updateNote(note: note, text: text);
+    }
+  }
+// dispose the data and widgets
+  @override
+  void dispose() {
+    deleteNoteIfEmpty();
+    saveNoteIfNotempty();
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("New Note"),
       ),
-      body: const Text("write your notes here."),
+      body: FutureBuilder(
+        future: createNote(),
+        builder: (context,snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.done:
+              _note = snapshot.data;
+              _setupTextControllerListener(); // start listening
+              return TextField(
+                controller: _textController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: "Enter your notes here"
+                ),
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        }
+        ),
     );
   }
 }
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -16,7 +16,7 @@ class _NotesViewState extends State<NotesView> {
   late final NotesService _notesService;
   String get userEmail => AuthService.firebase().currentUser!.email!;
   @override
-  void initState(){
+  void initState() {
     _notesService = NotesService();
     _notesService.open();
     super.initState();
@@ -27,86 +27,93 @@ class _NotesViewState extends State<NotesView> {
     _notesService.close();
     super.dispose();
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(title: const Text("Your Notes"),
-      actions: [
-        // the logout option in the menu
-        IconButton(
-          onPressed: () {
-            Navigator.of(context).pushNamed(newNoteRoute); // move to new notes page
-          },
-         icon: const Icon(Icons.add) // add the + button
-         ),
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch(value){
-                case MenuAction.logout:
-                  final confirmation = await logOut(context);
-                  // devtools.log(confirmation.toString()); // prints the confirmation boolean
-                  if(confirmation){
-                    await AuthService.firebase().logout();
-                    Navigator.of(context).pushNamedAndRemoveUntil(loginRoute, (_)=> false); // move to the login page upon logout
-                  }
-                  break;
-              }
-            },
-            itemBuilder: (context) { return const [ // itembuilder will return only a list<popupmenuentry<MenuAction>>  type -> note that popupMenuItem is a subclass of popupMenuEntry as it extends popupMenuEntry
-                 PopupMenuItem(value: MenuAction.logout, child: Text("logout")), // the value passed here will be passed on to the onSelected() function defined above as the value parameter
+      appBar: AppBar(
+        title: const Text("Your Notes"),
+        actions: [
+          // the logout option in the menu
+          IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(newNoteRoute); // move to new notes page
+              },
+              icon: const Icon(Icons.add) // add the + button
+              ),
+          PopupMenuButton<MenuAction>(onSelected: (value) async {
+            switch (value) {
+              case MenuAction.logout:
+                final confirmation = await logOut(context);
+                // devtools.log(confirmation.toString()); // prints the confirmation boolean
+                if (confirmation) {
+                  await AuthService.firebase().logout();
+                  Navigator.of(context).pushNamedAndRemoveUntil(loginRoute,
+                      (_) => false); // move to the login page upon logout
+                }
+                break;
+            }
+          }, itemBuilder: (context) {
+            return const [
+              // itembuilder will return only a list<popupmenuentry<MenuAction>>  type -> note that popupMenuItem is a subclass of popupMenuEntry as it extends popupMenuEntry
+              PopupMenuItem(
+                  value: MenuAction.logout,
+                  child: Text(
+                      "logout")), // the value passed here will be passed on to the onSelected() function defined above as the value parameter
             ];
-           }
-          )
+          })
         ],
       ),
-      body: FutureBuilder(future: _notesService.getOrCreateUser(email: userEmail),
-       builder: (context,snapshot){
-        switch(snapshot.connectionState){
-          case ConnectionState.done:
-            return StreamBuilder(
-              stream: _notesService.allNotes,
-              builder: (context,snapshot){
-                switch(snapshot.connectionState){
-                  case ConnectionState.waiting:
-                    return const Text("waiting for response.");
-                  default:
-                    return const CircularProgressIndicator();
-                }
-              },
-             );
-          default:
-            return const CircularProgressIndicator();
-        }
-       }
-       ),
+      body: FutureBuilder(
+          future: _notesService.getOrCreateUser(email: userEmail),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return StreamBuilder(
+                  stream: _notesService.allNotes,
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      // implicit follow through - no break b/w two cases so they follow through the next case
+                      case ConnectionState.waiting:
+                      case ConnectionState.active:
+                        return const Text("waiting for response.");
+                      default:
+                        return const CircularProgressIndicator();
+                    }
+                  },
+                );
+              default:
+                return const CircularProgressIndicator();
+            }
+          }),
     );
   }
 }
 
 // implementation of the logout confirmation dialog
-Future<bool> logOut(BuildContext context){
+Future<bool> logOut(BuildContext context) {
   return showDialog<bool>(
-        context : context,
-        builder: (context){
-        return AlertDialog(title: const Text('Logout'),
-            content: const Text("You really want to log out?"),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                  },
-                  child: const Text("Cancel")
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                  },
-                  child: const Text("Log Out")
-                ),
-              ],
-            );
-      },
-  ).then((value) => value ?? false); // since this is an optional future we make sure to return a future by checking for the null value return
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Logout'),
+        content: const Text("You really want to log out?"),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text("Cancel")),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text("Log Out")),
+        ],
+      );
+    },
+  ).then((value) =>
+      value ??
+      false); // since this is an optional future we make sure to return a future by checking for the null value return
 }
